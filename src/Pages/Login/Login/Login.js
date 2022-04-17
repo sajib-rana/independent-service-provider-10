78e35ac2c7ceb0d8f1c2b576c284e29b35a7acec
sajib-rana <sajibrana444@gmail.com>
2022-04-18 00:35:16 +0600

    toggle register and login

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -23,7 +23,7 @@ const Login = () => {
         <Form onSubmit={handleSubmit}>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
-            <Form.Control ref={emailRef} type="email" placeholder="Enter email" />
+            <Form.Control ref={emailRef} type="email" placeholder="Enter email" required/>
             <Form.Text className="text-muted">
               We'll never share your email with anyone else.
             </Form.Text>
@@ -31,7 +31,7 @@ const Login = () => {
 
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
-            <Form.Control ref={passwordRef} type="password" placeholder="Password" />
+            <Form.Control ref={passwordRef} type="password" placeholder="Password" required/>
           </Form.Group>
           <Form.Group className="mb-3" controlId="formBasicCheckbox">
             <Form.Check type="checkbox" label="Check me out" />
--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import './Register.css'
 
 const Register = () => {
+    const navigate = useNavigate()
+
+    const navigateLogin = () =>{
+        navigate('/login')
+    }
+    const handleRegister = event =>{
+        event.preventDefault();
+    }
     return (
-        <div>
-            <h1>this is register</h1>
+        <div className='register-info'>
+            <h1>Please Register</h1>
+            <form onSubmit={handleRegister}>
+                <input type="text" name="name" id=""  placeholder='Your name '/>
+                
+                <input type="email" name="email" id="" placeholder='Your email' required/>
+               
+                <input type="password" name="password" id="" placeholder='Your password' required/>
+                <input type="submit" value="Register" />
+                <p>Already have an account ? <Link to='/login' className='text-danger text-decoration-none' onClick={navigateLogin}>Please Login</Link></p>
+                
+            </form>
         </div>
     );
 };
