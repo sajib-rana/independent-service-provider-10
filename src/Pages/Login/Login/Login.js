2bd33087187d985021e3f987f066768e47f8f182
sajib-rana <sajibrana444@gmail.com>
2022-04-18 06:50:46 +0600

    login with google

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -3,6 +3,7 @@ import { Button, Form } from 'react-bootstrap';
 import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import auth from '../../../firebase.init';
+import SocialLogin from './SocialLogin/SocialLogin';
 
 const Login = () => {
     const emailRef = useRef('')
@@ -35,15 +36,9 @@ const Login = () => {
         <h1 className='text-primary text-center'>Please Login</h1>
         <Form onSubmit={handleSubmit}>
           <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
             <Form.Control ref={emailRef} type="email" placeholder="Enter email" required/>
-            <Form.Text className="text-muted">
-              We'll never share your email with anyone else.
-            </Form.Text>
           </Form.Group>
-
           <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
             <Form.Control ref={passwordRef} type="password" placeholder="Password" required/>
           </Form.Group>
           <Form.Group className="mb-3" controlId="formBasicCheckbox">
@@ -54,6 +49,7 @@ const Login = () => {
           </Button>
         </Form>
         <p>New to super body fitness? <Link to='/register' className='text-danger text-decoration-none' onClick={navigateRegister}>Please Register</Link></p>
+        <SocialLogin></SocialLogin>
       </div>
     );
 };
--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -3,6 +3,7 @@ import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
 import auth from '../../../firebase.init';
 import { Link, useNavigate } from 'react-router-dom';
 import './Register.css'
+import SocialLogin from '../Login/SocialLogin/SocialLogin';
 
 const Register = () => {
     const [
@@ -36,9 +37,10 @@ const Register = () => {
                
                 <input type="password" name="password" id="" placeholder='Your password' required/>
                 <input type="submit" value="Register" />
-                <p>Already have an account ? <Link to='/login' className='text-danger text-decoration-none' onClick={navigateLogin}>Please Login</Link></p>
                 
             </form>
+            <p>Already have an account ? <Link to='/login' className='text-danger text-decoration-none' onClick={navigateLogin}>Please Login</Link></p>
+            <SocialLogin></SocialLogin>
         </div>
     );
 };
